a3fb44036822f7133a89a2abe1486b76c6e506da
Bakhuya Milka Wanga <bakhuyamilka@gmail.com>
2023-04-05 21:46:51 -0700

    Retrieve value with sessions

--- Home.php
+++ Home.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+
+
 $adm=$_SESSION['adm'];
 $query1="SELECT * FROM student WHERE  student_id LIKE '$adm'";
 
@@ -10,6 +12,7 @@ if($query2){
 
 $data=mysqli_fetch_assoc($query2);
 print_r("Hi, " .($data['student_fname']));
+
 
 }
 
